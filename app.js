4c68b3e68a774ad336e992a05874668fce7dc3c8
Niemur Rahman <nrnaeim@gmail.com>
2025-01-16 12:46:45 +0600

    Main file app.js is created and imported dependencies and app object created

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+ * Title: Spotify Scrapper
+ * Description: This project is build to learning purpose of web scrapping with ethical practise of web scrapping
+ * Author: Niemur Rahman
+ * Email: nrnaeim@gmail.com
+ * Date: 16/01/2025
+ *
+ */
+//Dependencies
+import puppeteer from "puppeteer";
+
+//Module Scaffolding
+export const app = {};
