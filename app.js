9b7b4915fbf0b250e8e47dc64000b333f9e43da1
Niemur Rahman <nrnaeim@gmail.com>
2025-01-18 22:11:59 +0600

    selectors, logInInfo, handlers, helpers, inputs imported log in checker script added, looping through album url scrolling and data collection after each scroll added then data writter function called

--- app.js
+++ app.js
@@ -7,11 +7,18 @@
  *
  */
 //Dependencies
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 import { browserConfig } from "./lib/helpers/browserConfig.js";
+import { selectors } from "./lib/helpers/selectors.js";
+import { logInInfo } from "./secretInfo/logIn.js";
+import { handlers } from "./lib/handlers/handlers.js";
+import { helpers } from "./lib/helpers/helpers.js";
+import { inputs } from "./inputs.js";
 
 //Module Scaffolding
 export const app = {};
+
+app.data = [];
 app.mainFunction = async () => {
   const browser = await puppeteer.launch({
     executablePath: browserConfig.executablePathe,
@@ -21,6 +28,44 @@ app.mainFunction = async () => {
     args: ["--start-maximized"],
     devtools: false,
   });
-
   const page = await browser.newPage();
+  await page.setDefaultTimeout(0);
+
+  await page.goto("https://open.spotify.com/", {
+    waitUntil: "networkidle0",
+  });
+  await handlers.delayFunction(500, 1500);
+  //checking already logged in or not
+  if ((await page.$(selectors.logInPage)) !== null) {
+    await page.click(selectors.logInPage);
+    await page.waitForSelector(selectors.userName, { visible: true });
+    await page.type(selectors.userName, logInInfo.userName, { delay: 10 });
+    await page.type(selectors.password, logInInfo.password, { delay: 10 });
+    await page.click(selectors.logInBtn);
+    await handlers.delayFunction(500, 1000);
+  }
+
+  for (let i = 0; i < inputs.length; i++) {
+    await page.goto(inputs[i].allbumURL, { waitUntil: "networkidle0" });
+
+    let scrolled = 0;
+    while (true) {
+      scrolled += 500;
+      const scrollHeight = await handlers.scrollFunction(page, scrolled);
+      await handlers.delayFunction(1000, 3000);
+      let dataArr = await helpers.collector(page);
+      dataArr.forEach((element) => {
+        element.artist = inputs[i].artist;
+      });
+      app.data.push(...dataArr);
+      if (scrolled >= scrollHeight) {
+        break;
+      }
+    }
+    helpers.dataWritter(app.data);
+    console.log(`${i + 1} of ${inputs.length} Completed successfully`);
+  }
+  console.log(`Congratulations! Your JOB Completed successfully`);
+  await browser.close();
 };
+app.mainFunction();
