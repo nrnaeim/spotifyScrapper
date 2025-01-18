992a419c5740fe6075adced287c40daf9d44a9a0
Niemur Rahman <nrnaeim@gmail.com>
2025-01-18 21:58:47 +0600

    Uploading  secretInfo/logIn.js to remote repo

---
+++ secretInfo/logIn.js
@@ -0,0 +1,14 @@
+/*
+ * Title: Secret information
+ * Description: Log In detais and other secret information
+ * Author: Niemur Rahman
+ * Email: nrnaeim@gmail.com
+ * Date: 16/01/2025
+ *
+ */
+//Dependencies
+
+//Module Scaffolding
+export const logInInfo = {};
+logInInfo.userName = "";
+logInInfo.password = "";
